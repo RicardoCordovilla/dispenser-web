05462c5768fdc9d6edd52702bcdcc85e6d5d3fe2
RicardoCordovilla <ricardocordovillaf@gmail.com>
2023-07-19 01:18:08 -0500

    animate manual

--- encenderDispositivo.js
+++ encenderDispositivo.js
@@ -1,4 +1,13 @@
-function encenderDispositivo() {
+const btnEncender = document.getElementById("btnEncender")
+
+btnEncender.addEventListener("click", encenderDispositivo)
+
+
+function encenderDispositivo(e) {
+  e.preventDefault()
+  btnEncender.classList.add("animate__animated")
+  btnEncender.classList.add("anianimate__bounce")
+  btnEncender.disabled=true
   console.log('encender')
   fetch('https://dispenser-api-production.up.railway.app/api/v1/secuencias/u/1', {
     method: 'PATCH',
@@ -6,6 +15,13 @@ function encenderDispositivo() {
       'Content-type': 'application/json; charset=UTF-8',
     },
   })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json)
+    setTimeout(() => {
+        btnEncender.disabled=false
+        btnEncender.classList.remove("animate__animated")
+        btnEncender.classList.remove("anianimate__bounce")
+      }, 1000);
+    });
 }
